9b5823fb0975c9609290c4d9d328438648c8ae41
dylanmiley <dylmiley.dcmiley@ucdavis.edu>
2025-07-23 16:09:39 -0400

    minor changes to orientation metric

--- orientation-metric/orientation-dev.py
+++ orientation-metric/orientation-dev.py
@@ -1,8 +1,8 @@
 import sys
-sys.path.append('../state-space/')
-
+sys.path.append('../state-space')
 import numpy as np
-import master_wass_metric_binary as mwmb
+import matplotlib.pyplot as plt
+# import master_wass_metric_binary as mwmb
 
 def quatmult(q1, q2):
     ### quaternion multiplication ### 
@@ -23,9 +23,9 @@ def quatmult(q1, q2):
 
 def misorientation(q1, q2):
     ### calculation of rotation quaternion for q1 and q2 ###
-    r = quatmult(q2, q1)
-    q2[1:] *= -1
-    r = quatmult(r, q2)
+    q2inv = np.copy(q2)
+    q2inv[1:] *= -1
+    r = quatmult(q1, q2inv)
     return r
 
 
@@ -36,13 +36,15 @@ def create_FID(G, unique_q, n):
     for quats in unique_q:
         where_q[np.where(G == quats)[:2]] = FID
         FID += 1
+    plt.imshow(where_q)
+    plt.show()
     return where_q
 
 
 def texture_graph(A,B):
     ### the graph for orientation difference for two windows, A and B ###
     n = np.shape(A)[0]
-    G = np.zeros((n**2, n**2,4))
+    G = np.zeros((n**2, n**2))
 
     ### we only want to compute disorientation for unique pairs ###
     uniqueA = np.unique(A.reshape(-1,4), axis=0)
@@ -51,13 +53,14 @@ def texture_graph(A,B):
     nB = np.shape(uniqueA)[0]
     fidA = create_FID(A, uniqueA, n)
     fidB = create_FID(B, uniqueB, n)
-    print(fidA + 2*fidB)
-    misorientations = np.zeros((nA,nB,4))
+    plt.imshow(fidA + 2*fidB)
+    plt.show()
+    misorientations = np.zeros((nA,nB))
     i = 0
     for quatA in uniqueA:
         j = 0
         for quatB in uniqueB:
-            misorientations[i,j] = misorientation(quatA, quatB)
+            misorientations[i,j] = misorientation(quatA, quatB)[0]
         j+=1
         i+=1
     ### assign disorientation to the full graph ###
@@ -67,7 +70,7 @@ def texture_graph(A,B):
             for k in range(n):
                 for l in range(n):
                     b = k*n + l
-                    G[a,b] = misorientations[fidA[i,j], fidB[k,l]]
+                    G[a,b] = 2*np.arccos(misorientations[fidA[i,j], fidB[k,l]])
 
     return G
 
@@ -87,6 +90,7 @@ if __name__ == '__main__':
     A = np.ones((l,l,4))
     A[:,:]*=q1
     B = np.ones((l,l,4))
+    B[:,:]*=q1
     ijA = rng.integers(0, l, size=(rng.integers(0,l**2), 2))
     for ij in ijA:
         A[ij[0], ij[1], :] = q2
@@ -95,3 +99,8 @@ if __name__ == '__main__':
         B[ij[0], ij[1], :] = q2
 
     G = texture_graph(A,B)
+
+print(360/np.pi*np.arccos(misorientation(q1, q2)[0]))
+#q1a = Quaternion(q1)
+#q2a = Quaternion(q2)
+#print(Quaternion.absolute_distance(q1a,q2a)*360/np.pi)
